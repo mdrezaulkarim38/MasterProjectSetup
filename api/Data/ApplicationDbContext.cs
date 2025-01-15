ce3ad01407a533a92d5e3f79260fe3ceed914734
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-15 21:34:44 +0600

    application Db context added

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+namespace api.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+}
