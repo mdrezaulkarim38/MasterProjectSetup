f7c300a4c815f26e0d2c354113f47f8437c0a5d5
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-16 08:32:59 +0600

    All work fine

--- api/Controllers/AuthController.cs
+++ api/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace api.Controllers;
 
@@ -24,9 +28,9 @@ public class AuthController : ControllerBase
 
         var user = new AppUser
         {
-            UserName = model.Email,
+            UserName = model.Username,
             Email = model.Email,
-            Name = model.Username,
+            Name = model.Name,
         };
         var result = await _userManager.CreateAsync(user, model.Password!);
         if (result.Succeeded)
@@ -37,5 +41,38 @@ public class AuthController : ControllerBase
             ModelState.AddModelError(error.Code, error.Description);
         
         return BadRequest(ModelState);
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto model)
+    {
+        var user = await _userManager.FindByEmailAsync(model.Email);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            return Unauthorized(new { Message = "Invalid credentials" });
+
+        var token = GenerateJwtToken(user);
+        return Ok(new { Token = token });
+    }
+
+    private string GenerateJwtToken(AppUser user)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("LastName", user.Name)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("2c1783ed-a83b-42c8-b5d8-db6389cac45a"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "http://localhost:5035",
+            audience: "http://localhost:5035",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
--- api/Dtos/Auth/LoginDto.cs
+++ api/Dtos/Auth/LoginDto.cs
@@ -2,14 +2,9 @@
 
 namespace api.Dtos.Auth;
 
-public class RegisterDto
+public class LoginDto
 {
     [Required]
-    public string? Name { get; set; }
-    [Required]
-    public string? Username { get; set; }
-    [Required]
-    [EmailAddress]
     public string? Email { get; set; }
     [Required]
     public string? Password { get; set; }
