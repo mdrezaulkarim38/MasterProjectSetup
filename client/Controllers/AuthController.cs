00f43ab26e26665e8a9078aa05587b8d21607f7f
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-19 21:27:23 +0600

    Add Login file

---
+++ client/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace client.Controllers;
+
+public class AuthController : Controller
+{
+    public AuthController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
