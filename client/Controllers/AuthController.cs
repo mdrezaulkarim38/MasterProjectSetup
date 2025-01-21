35f5137df84846b4e2b6d7c8d0200e23cdeb2523
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-21 20:37:50 +0600

    Now set the model data

--- client/Controllers/AuthController.cs
+++ client/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using client.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace client.Controllers;
 
@@ -15,7 +16,7 @@ public class AuthController : Controller
     }
 
     [HttpPost]
-    public IActionResult Login(string? email, string? password)
+    public IActionResult Login(User model)
     {
         if (ModelState.IsValid)
         {
